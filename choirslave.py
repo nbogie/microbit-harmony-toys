9832809e7b4968c9b6a5b82fd0e26fc857be6adf
Neill Bogie <neillbogie@googlemail.com>
2017-02-28 21:43:58 +0000

    add slave ability to go to neighbour notes (poorly)

--- choirslave.py
+++ choirslave.py
@@ -18,9 +18,50 @@ chords = {
     'VI': ["A3:1", "C#5:1", "E5:1", "G5:1"], 
     'vii': ["B3:1", "D5:1", "F5:1", "A5:1"]
     }
+scale = "C D E F G A B".split(' ')
+
+def assertEq(a, b):
+    if a!=b:
+        display.scroll("AF." + str(a) + " != " + str(b));
+        microbit.panic(1)
 
 
+def noteOf(noteWord):
+    #TODO: consider also flats and sharps
+    return noteWord[0]
 
+def octaveOf(noteWord):
+    if (noteWord[0] == 'R'):
+        return ''
+    return int(noteWord.replace('#', "").replace('b', '')[1])
+    
+
+def neighbour(note, octave, offset):
+    if offset == 0:
+        return note + str(octave)
+
+    foundIx = scale.index(note)
+    nIx = foundIx + offset
+    if nIx >= len(scale):
+        nIx = nIx - len(scale)
+        octave = octave + 1
+    if nIx < 0:
+        nIx = nIx + len(scale)
+        octave = octave - 1
+
+    return scale[nIx] + str(octave)
+
+    
+assertEq(3, octaveOf("B3:1"))
+assertEq(5, octaveOf("C#5:1"))
+assertEq('C', noteOf("C#5:1"))
+assertEq('C', noteOf("C5:1"))
+
+assertEq('G3', neighbour('A', 3, -1))
+assertEq('B3', neighbour('A', 3, 1))
+
+
+neighbourOffset = 0
 beatIx = 0
 partIx = 0
 chordName = '' #received from master
@@ -32,6 +73,8 @@ def playNextNote():
     noteName = chords[chordName][partIx]
     if (beatIx != 0 and random.random() > 0.9):
         noteName = "R:1"
+    elif neighbourOffset != 0:
+        noteName = neighbour(noteOf(noteName), octaveOf(noteName), neighbourOffset)
     music.play(noteName)
     beatIx = beatIx + 1
 
@@ -43,6 +86,21 @@ def incPartIx(offset):
     if (partIx < 0):
         partIx = 3
 
+
+def neighbourOffsetFromGesture():
+    #gesture = accelerometer.current_gesture()
+    global neighbourOffset
+    tilt = accelerometer.get_x()
+
+    if tilt < 20:
+        neighbourOffset = -1
+    elif tilt > 20:
+        neighbourOffset = 1
+    else:
+        neighbourOffset = 0;
+
+
+
 radio.on()
 display.show(Image.GHOST)
 while True:
@@ -52,7 +110,9 @@ while True:
         #delays things, at least when double digits
         #display.show(msg)
         chordName = msg
-    
+
+    #neighbourOffsetFromGesture()    
+
     if (chordName != None and chordName != ''):
         playNextNote()
 
