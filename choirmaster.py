94a7f329aad0c0287e5d9f06c2581e6ac430fbd9
Neill Bogie <neillbogie@googlemail.com>
2017-02-28 19:52:45 +0000

    both ostinato through chords, part is changeable

--- choirmaster.py
+++ choirmaster.py
@@ -1,7 +1,7 @@
 from microbit import *
 import music
 import radio
-
+import random
 chords = [
     ["C4:1", "E4:1", "G4:1", "B4:1"], 
     ["D4:1", "F4:1", "A4:1", "C5:1"], 
@@ -9,28 +9,51 @@ chords = [
     ["F4:1", "A4:1", "C5:1", "E5:1"], 
     ["G4:1", "B4:1", "D5:1", "F5:1"], 
     ["A4:1", "C5:1", "E5:1", "G5:1"], 
-    ["B4:1", "D5:1", "F5:1", "A5:1"]
+    ["B4:1", "D5:1", "F5:1", "A5:1"],
+
+    ["D4:1", "F#4:1", "A4:1", "C5:1"], 
+    ["E4:1", "G#4:1", "B4:1", "D5:1"], 
+    ["F4:1", "Ab4:1", "C5:1", "E5:1"], 
+    ["A4:1", "C#5:1", "E5:1", "G5:1"] 
     ]
 
-progIx = 0
-ix = 0
-progression = [1,4,5,4]
 
-display.show(Image.PACMAN)
-radio.on()
+progIx = 0
+beatIx = 0
+progression = [1,9,4,11,2,11,5]
+partIx = 0
+
 
 def incProgIx():
-    global progIx, ix
+    global progIx
     progIx = progIx + 1
-    display.show(str(progIx))
     if (progIx >= len(progression)):
-        progIx = 0;
-    ix = progression[progIx] - 1
+        progIx = 0;    
+    chordIx = progression[progIx] - 1
+    #delays things
+    #display.show(str(chordIx))
+    radio.send(str(chordIx))
 
-    
-def play():
-    radio.send(str(ix))
-    music.play(chords[ix])
+def playNextNote():
+    global beatIx
+    if (beatIx >= 16):
+        incProgIx()
+        beatIx = 0
+
+    chordIx = progression[progIx] - 1
+    noteName = chords[chordIx][partIx]
+    if (beatIx != 0 and random.random() > 0.8):
+        noteName = "R:1"
+    music.play(noteName)
+    beatIx = beatIx + 1
+
+def incPartIx(offset):
+    global partIx
+    partIx = partIx + offset
+    if (partIx > 3):
+        partIx = 0
+    if (partIx < 0):
+        partIx = 3
 
 def incIx(offset):
     global ix
@@ -42,13 +65,14 @@ def incIx(offset):
     display.show(str(ix + 1));
 
 
+display.show(Image.PACMAN)
+radio.on()
+
 while True:
-    play()
-    play()
-    play()
-    play()
-    incProgIx()
+    playNextNote()
+
+    if button_a.is_pressed():
+        incPartIx(-1)
 
     if button_b.is_pressed():
-        incProgIx()
-        play()
+        incPartIx(1)
--- choirslave.py
+++ choirslave.py
@@ -1,6 +1,7 @@
 from microbit import *
 import music
 import radio
+import random
 
 chords = [
     ["C4:1", "E4:1", "G4:1", "B4:1"], 
@@ -9,21 +10,49 @@ chords = [
     ["F4:1", "A4:1", "C5:1", "E5:1"], 
     ["G4:1", "B4:1", "D5:1", "F5:1"], 
     ["A4:1", "C5:1", "E5:1", "G5:1"], 
-    ["B4:1", "D5:1", "F5:1", "A5:1"]
-    ]
-ix = 0;
+    ["B4:1", "D5:1", "F5:1", "A5:1"],
 
-def play():
-    y = chords[ix][:]
-    y.reverse()
-    music.play(y)
+    ["D4:1", "F#4:1", "A4:1", "C5:1"], 
+    ["E4:1", "G#4:1", "B4:1", "D5:1"], 
+    ["F4:1", "Ab4:1", "C5:1", "E5:1"], 
+    ["A4:1", "C#5:1", "E5:1", "G5:1"] 
+    ]
+
+beatIx = 0
+partIx = 0
+chordIx = 0 #received from master
+
+def playNextNote():
+    global beatIx
+    if (beatIx >= 16):
+        beatIx = 0
+    noteName = chords[chordIx][partIx]
+    if (beatIx != 0 and random.random() > 0.9):
+        noteName = "R:1"
+    music.play(noteName)
+    beatIx = beatIx + 1
+
+def incPartIx(offset):
+    global partIx
+    partIx = partIx + offset
+    if (partIx > 3):
+        partIx = 0
+    if (partIx < 0):
+        partIx = 3
 
 radio.on()
 display.show(Image.GHOST)
-
 while True:
+    playNextNote()
+
     msg = radio.receive()
     if (msg != None):
-        display.show(msg)
-        ix = int(msg)
-        play()      
+        #delays things, at least when double digits
+        #display.show(msg)
+        chordIx = int(msg)
+
+    if button_a.is_pressed():
+        incPartIx(-1)
+
+    if button_b.is_pressed():
+        incPartIx(1)
